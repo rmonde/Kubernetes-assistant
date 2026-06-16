0eedf7ce6e010d08a5c4966bf65fa96434111992
rmonde <monde_rahul@yahoo.co.in>
2026-06-16 17:08:33 +0530

    Implement full RAG pipeline in 04_rag_pipeline.py with embedding, search, and prompt building functions; add session notes for 2026-06-16 detailing functionality and bugs fixed.

---
+++ 04_rag_pipeline.py
@@ -0,0 +1,88 @@
+from openai import AzureOpenAI
+from dotenv import load_dotenv
+from azure.search.documents import SearchClient
+from azure.search.documents.models import VectorizedQuery
+from azure.core.credentials import AzureKeyCredential
+import os
+
+load_dotenv()
+
+client = AzureOpenAI(
+    api_key=os.getenv("AZURE_OPENAI_API_KEY"),  
+    azure_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT"),
+    api_version=os.getenv("AZURE_OPENAI_API_VERSION")
+    )
+
+def embed(question):
+    response = client.embeddings.create(
+        input=question,
+        model="embedding",
+    )
+    print("Embedding created successfully!")
+    print(f" Length: {len(response.data[0].embedding)}")
+    return response.data[0].embedding
+
+def search(vector):
+    credential = AzureKeyCredential(os.getenv("AZURE_SEARCH_ADMIN_KEY"))
+    search_client = SearchClient(
+        endpoint=os.getenv("AZURE_SEARCH_ENDPOINT"),
+        index_name=os.getenv("AZURE_SEARCH_INDEX_NAME"),
+        credential=credential
+    )
+    # Define the Vectorized Query
+    vector_query = VectorizedQuery(
+        fields="content_vector",
+        k_nearest_neighbors=3,
+        vector=vector
+    )
+    results = search_client.search(
+        search_text=None,
+        vector_queries=[vector_query]
+    )
+    return results
+
+def build_prompt(question, chunk):
+    prompt = f"""
+    Question: {question}
+    Context: {"\n\n---\n\n".join(chunk)}
+    Answer:
+    """
+    print(prompt)
+    result = client.chat.completions.create(
+        model=os.getenv("AZURE_OPENAI_DEPLOYMENT"),
+        messages=[
+            {
+                "role": "system", 
+                "content": "You are a helpful assistant. Use ONLY the provided context to answer the user's question. "
+                "If the information is not in the context, say 'I do not know'.Do not add any information beyond what is in the context."
+            },
+            {
+                "role": "user", 
+                "content": prompt
+            }
+        ],
+        temperature=0.0,
+        max_tokens=500
+    )
+
+    return result.choices[0].message.content
+
+
+def main():
+    question = "My pod keeps restarting and the status shows OOMKilled ?"
+    embedding = embed(question)
+    results = search(embedding)
+
+    # Collect the content from the search results
+    # and create a common chunk to pass to the LLM
+    chunk = []
+    for result in results:
+        chunk.append(result["content"])
+        
+    
+    answer = build_prompt(question, chunk)
+    print(answer)
+
+
+if __name__ == "__main__":
+    main()
